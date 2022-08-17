1ff2c063e2fe26020d805302e11ebd1b346e3377
Pluto1243 <1243173286@qq.com>
2022-08-17 16:59:38 +0800

    8.15 2、有效的括号

---
+++ src/IsValid.java
@@ -0,0 +1,65 @@
+import java.util.Stack;
+
+/**
+ * 
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序 闭合。
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：s = "()"
+ * 输出：true
+ * 示例2：
+ *
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例3：
+ *
+ * 输入：s = "(]"
+ * 输出：false
+ * 示例4：
+ *
+ * 输入：s = "([)]"
+ * 输出：false
+ * 示例5：
+ *
+ * 输入：s = "{[]}"
+ * 输出：true
+ * 
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ **/
+public class IsValid {
+
+    public boolean isValid(String s) {
+        if (s.isEmpty()) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '{') {
+                stack.push('}');
+            } else if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.empty() || c != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
