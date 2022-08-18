c0e9e656dea7c8b1577a49646639b553b0159077
Pluto1243 <1243173286@qq.com>
2022-08-18 15:47:53 +0800

    8.18 最长回文串

---
+++ src/LongestPalindrome.java
@@ -0,0 +1,55 @@
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class LongestPalindrome {
+    public static String longestPalindrome(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int start = 0;
+        int end = 0;
+        int maxLen = 0;
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j < 3 || dp[i-1][j+1])) {
+                    dp[i][j] = true;
+                    int len = i - j + 1;
+                    if (maxLen < len) {
+                        start = j;
+                        end = i;
+                        maxLen = len;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static void main(String[] args) {
+        String s = "ccc";
+        System.out.println(longestPalindrome(s));
+    }
+}
