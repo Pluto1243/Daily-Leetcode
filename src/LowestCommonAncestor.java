19e9b15c383f3e5e19c01e427ff52bf36647c6a8
Alex <1243173286@qq.com>
2022-08-19 23:17:18 +0800

    8.19 二叉树的最近公共祖先

---
+++ src/LowestCommonAncestor.java
@@ -0,0 +1,81 @@
+import java.util.HashMap;
+import java.util.TreeSet;
+
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出：3
+ * 解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出：5
+ * 解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+ * 示例 3：
+ * <p>
+ * 输入：root = [1,2], p = 1, q = 2
+ * 输出：1
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [2, 105] 内。
+ * -109 <= Node.val <= 109
+ * 所有 Node.val 互不相同 。
+ * p != q
+ * p 和 q 均存在于给定的二叉树中。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class LowestCommonAncestor {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 终止条件 找到了p或q 或者已经到null节点了
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        // 找左子树和右子树
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 左边和右边都不存在
+        if (left == null && right == null) {
+            return null;
+        }
+        // 左边为null，说明 p q都在右边
+        if (left == null) {
+            return right;
+        }
+        // 右边为null，说明 p q都在左边
+        if (right == null) {
+            return left;
+        }
+        // 该节点下的左右都有p、q，说明该节点为公共节点
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        lowestCommonAncestor(node, new TreeNode(1), new TreeNode(2));
+    }
+}
