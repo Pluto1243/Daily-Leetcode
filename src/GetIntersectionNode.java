9b6ce1312c7d97cd2729166d35b50167454d8216
Alex <1243173286@qq.com>
2022-08-22 09:28:42 +0800

    8.22 两个单链表相交的起始节点

---
+++ src/GetIntersectionNode.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/
+ **/
+public class GetIntersectionNode {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
