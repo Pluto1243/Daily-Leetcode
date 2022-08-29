5a03fe7e2d59ed1ac1383a10cd6e48d0eca91aba
Alex <1243173286@qq.com>
2022-08-29 23:49:13 +0800

    8.29 ReorderList

---
+++ src/ReorderList.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+ *
+ * L0 → L1 → … → Ln - 1 → Ln
+ * 请将其重新排列后变为：
+ *
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * 
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[1,4,2,3]
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[1,5,2,4,3]
+ * 
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/reorder-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class ReorderList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        //存到 list 中去
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        //头尾指针依次取元素
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            //偶数个节点的情况，会提前相遇
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+
+}
