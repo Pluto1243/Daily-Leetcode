87b5e9dd729439776288cc5c15ef707b9aa8bb36
Alex <1243173286@qq.com>
2022-09-02 23:59:33 +0800

    9.2 BinarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -29,7 +29,7 @@ public class BinarySearch {
     public static int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = (right - left) / 2 + left;
             if (nums[mid] == target) {
                 return mid;
