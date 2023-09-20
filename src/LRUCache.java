df57f0a5aca0e2335a85578ed5a83c22aee8403b
Alex <Y001980@ylwl.com>
2023-09-20 09:59:19 +0800

    feat: 2023/9/19 LRU缓存

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -1,5 +1,4 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
  * LRU缓存
@@ -9,29 +8,103 @@ import java.util.Map;
  * @author wangjie
  * @date 19:22 2023年09月19日
  **/
-public class LRUCache extends LinkedHashMap<Integer, Integer> {
+public class LRUCache {
+
+    HashMap<Integer, ListNode> map = new HashMap<>();
+
+    ListNode head = new ListNode();
+
+    ListNode tail = new ListNode();
 
     int capacity;
 
+    int size;
+
     public LRUCache(int capacity) {
-        super(capacity, 0.75f, true);
         this.capacity = capacity;
+        head.next = tail;
+        tail.pre = head;
     }
 
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        int value = -1;
+        if (map.containsKey(key)) {
+            ListNode node = map.get(key);
+            moveToHead(node);
+            value = node.value;
+        }
+        return value;
     }
 
     public void put(int key, int value) {
-        super.put(key, value);
+        if (map.containsKey(key)) {
+            ListNode listNode = map.get(key);
+            listNode.value = value;
+            moveToHead(listNode);
+        } else {
+            ListNode newNode = new ListNode(key, value);
+            addToHead(newNode);
+            map.put(key, newNode);
+            size++;
+            if (size > capacity) {
+                ListNode remove = removeTail();
+                map.remove(remove.key);
+                size--;
+            }
+        }
     }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+    public void removeNode(ListNode node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    public void addToHead(ListNode node) {
+        node.pre = head;
+        node.next = head.next;
+        head.next.pre = node;
+        head.next = node;
+    }
+
+    public void moveToHead(ListNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    public ListNode removeTail() {
+        ListNode tail = this.tail.pre;
+        removeNode(tail);
+        return tail;
+    }
+
+
+
+    /**
+     * Node双向链表
+     *
+     * @author wangjie
+     * @date 09:11 2023年09月20日
+     **/
+    class ListNode {
+
+        ListNode pre;
+
+        ListNode next;
+
+        int key;
+
+        int value;
+
+        public ListNode() {
+
+        }
+
+        public ListNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
