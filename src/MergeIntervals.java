4c63a9883737117e1966292284bbe4dd750be06a
Alex <Y001980@ylwl.com>
2023-10-09 20:27:05 +0800

    feat: 2023/10/9 MergeIntervals

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -32,18 +32,17 @@ public class MergeIntervals {
 
     public static int[][] merge(int[][] intervals) {
         int[][] res = new int[intervals.length][2];
-        // 先按照区间起始位置排序
-        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
-        // 遍历区间
+        Arrays.sort(intervals, (v0, v1) -> v0[0] - v1[0]);
         int idx = -1;
-        for (int[] interval: intervals) {
-            // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
-            // 则不合并，直接将当前区间加入结果数组。
-            if (idx == -1 || interval[0] > res[idx][1]) {
-                res[++idx] = interval;
+        for (int[] it: intervals
+             ) {
+            // 当前区间的最大值小于下一区间的最小值，则没有重叠
+            if (idx == -1 || res[idx][1] < it[0]) {
+                idx++;
+                res[idx] = it;
             } else {
-                // 反之将当前区间合并至结果数组的最后区间
-                res[idx][1] = Math.max(res[idx][1], interval[1]);
+                // 否则找到重叠的最大部分
+                res[idx][1] = Math.max(res[idx][1], it[1]);
             }
         }
         return Arrays.copyOf(res, idx + 1);
